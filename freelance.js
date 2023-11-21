1d884139c1f637bca38a133bde0fb4d09dfeddb0
xavier <xaviergomez@live.com>
2023-11-21 16:48:51 -0700

    finished project

--- freelance.js
+++ freelance.js
@@ -1,54 +1,66 @@
-const names = ["Dr.Slice","Mr.Pressure","Prof.Possibility"];
-const prices =[20, 30, 44, 75];
-const occupations = ["gardener","programmer","teacher","driver"]
-const maxPerson = 5;
 
-const freeLancers = [
+//name price and job holders
+const names = ["Dr.Slice","Mr.Pressure","Prof.Possibility", "Carol","AwildSeacow", "Xavier"];
+const prices =[20, 30, 50, 75];
+const occupations = ["gardener","programmer","teacher","driver"]
+const maxPerson = 20
+//set state for a person 
+const freeLanceWriters = [
   {
     name: "Dr.Pressure",
     price: 20,
-    occupation:"gardener"
+    occupation:"Gardener"
   },
   {
     name: "Dr.Slive",
     price: 30,
-    occupation:"teacher",
+    occupation:"Teacher",
   },
-  
-];
-
-
- const addFreeLanceInterval = setInterval(addFreeLance,1000)
-
-
-/*
-  
-  function render(){
-    const persons = document.querySelector("#persons");
-    const personElements = freeLancers.map((person)=> {
-      const element = document.createElement("li");
-      element.classList.add(person.name, person.price, person.occupation);
-      return element;
-    });
-
-    persons.replaceChildren(...personElements);
- 
+  {
+    name: "Prof.Possibility",
+    price: 75,
+    occupation:"Programmer",
   }
-*/
+];
+const addFreelancerIntervalId = setInterval(addFreeLance, 1000);
+render();
 
-
-
+  //Create render function to rdnder a persons name job and price
+  function render(){
+    const averagePrice = document.querySelector("#average-price");
+    const freeLanceWritersEl = document.querySelector("tbody");
+    const template = freeLanceWriters.map((person)=> {
+      const tr = document.createElement("tr");
+      const td1 =document.createElement("td");
+      td1.textContent = person.name;
+      const td2 =document.createElement("td");
+      td2.textContent = person.occupation;
+      const td3 =document.createElement("td");
+      td3.textContent = `$${person.price}`;
+      tr.append(td1,td2,td3);
+      return tr;
+    });
+    freeLanceWritersEl.replaceChildren(...template);
+    const newAverage = calculateAveragePrice();
+    averagePrice.innerText = `$${newAverage}`;
+  }
+  //call the render function to make it show 
+ render()
+//creating a random person from my holders
   function addFreeLance(){
     const name = names[Math.floor(Math.random() * names.length)];
     const price = prices[Math.floor(Math.random() * prices.length)];
     const occupation = occupations[Math.floor(Math.random() * occupations.length)];
-    if (freeLancers.length >= maxPerson) {
+    if (freeLanceWriters.length >= maxPerson) {
       clearInterval(addFreeLanceInterval);
     }
-
-    freeLancers.push({ name, price, occupation });
-    console.log(freeLancers)
-  
+    freeLanceWriters.push({ name, price, occupation });
+    //call the render fucntion to show 
+  render();
   }
 
-  console.log()
+  function calculateAveragePrice() {
+    const total = freeLanceWriters.reduce(
+      (subtotal, current) => subtotal + current.price,0);
+    return Math.round(total / freeLanceWriters.length);
+    }
